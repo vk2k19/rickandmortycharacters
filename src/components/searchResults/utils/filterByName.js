dceccd21d018bd84251b6adbbc2c9e7682374d9f
Vijayendra Kumar <55515410+vk2k19@users.noreply.github.com>
2024-01-01 18:12:58 +0530

    Delete src directory
    Moved to app and refactored

--- src/components/searchResults/utils/filterByName.js
+++
@@ -1,8 +0,0 @@
-const filterByName = (results, name) => {
-  let query = name.trim().toLowerCase();
-  if(!query) {
-    return results;
-  }
-  return results.filter(result => result.name.trim().toLowerCase().startsWith(query))
-}
-export default filterByName;
