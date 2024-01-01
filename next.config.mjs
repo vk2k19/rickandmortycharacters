31f02d1f2daf76bc8ec8c018228ebd8d7c978375
Vijayendra Kumar <55515410+vk2k19@users.noreply.github.com>
2024-01-01 18:29:19 +0530

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,18 +1,17 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
-  images: {
-		deviceSizes: [320, 375, 480, 768, 1024, 1500],
-		dangerouslyAllowSVG: true,
-		remotePatterns: [
-			{
-				protocol: 'https',
-				hostname: 'rickandmortyapi.com',
-				port: '',
-				pathname: '/**',
-			},
-		],
+	reactStrictMode: true,
+	output: 'export',
+	images: {
+	  	deviceSizes: [320, 375, 480, 768, 1024, 1500],
+	  	dangerouslyAllowSVG: true,
+	  	remotePatterns: [{
+			   protocol: 'https',
+		   	hostname: 'rickandmortyapi.com',
+		   	port: '',
+		   	pathname: '/**',
+	  	}],
 	},
 }
-
+  
 export default nextConfig
